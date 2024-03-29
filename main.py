944f573202d9305a6c11ab107e50dc1cad44eb16
Aarón Medina <a.medina.v.96@hotmail.com>
2024-03-29 12:47:02 +0100

    [feat] Add title, icon and background color

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ pygame.init()
 
 pantalla = pygame.display.set_mode((800, 600))
 
-executing = True
+pygame.display.set_caption("Invasion espacial")
+pygame.display.set_icon(pygame.image.load('ovni.png'))
 
+executing = True
 while executing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             executing = False
+
+    pantalla.fill((205, 144, 228))
+    pygame.display.update()
