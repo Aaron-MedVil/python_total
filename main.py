04509b3f7f4bb9db2fa7cb3ea84bd764e8b8cfce
Aarón Medina <a.medina.v.96@hotmail.com>
2024-03-29 12:59:50 +0100

    [feat] Create img folder. Add player to screen

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import pygame
 
 pygame.init()
 
-pantalla = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("Invasion espacial")
-pygame.display.set_icon(pygame.image.load('ovni.png'))
+pygame.display.set_icon(pygame.image.load('img/ovni.png'))
+
+pj_img = pygame.image.load("img/rocket.png")
+pj_x = 368
+pj_y = 536
+
+def player():
+    screen.blit(pj_img, (pj_x, pj_y))
 
 executing = True
 while executing:
+    screen.fill((205, 144, 228))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            executing = False
-
-    pantalla.fill((205, 144, 228))
+            executing = False    
+    
+    player()
     pygame.display.update()
