cea7250a4a77ecb95583f0a1929bb6161a459811
Aarón Medina <a.medina.v.96@hotmail.com>
2024-03-29 13:22:33 +0100

    [feat] Add player movement

--- main.py
+++ main.py
@@ -10,17 +10,37 @@ pygame.display.set_icon(pygame.image.load('img/ovni.png'))
 pj_img = pygame.image.load("img/rocket.png")
 pj_x = 368
 pj_y = 536
+pj_x_change = 0
 
-def player():
-    screen.blit(pj_img, (pj_x, pj_y))
+def player(x, y):
+    screen.blit(pj_img, (x, y))
 
 executing = True
 while executing:
     screen.fill((205, 144, 228))
-    
+        
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
-            executing = False    
+            executing = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                pj_x_change = -3
+            if event.key == pygame.K_RIGHT:
+                pj_x_change = 3
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                pj_x_change = 0
     
-    player()
+    pj_x += pj_x_change
+    
+    if pj_x <= 0:
+        pj_x = 0
+    
+    if pj_x >= 736:
+        pj_x = 736
+    
+    player(pj_x, pj_y)
     pygame.display.update()
